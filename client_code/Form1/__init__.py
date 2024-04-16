f612984b5d9748a39b5bc796fdd5f8535677c72e
royalgarg36@gmail.com <royalgarg36@gmail.com>
2024-04-16 05:48:22 +0000

    Created custom package dependencies; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,15 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run before the form opens.
+
+  def run_code_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('func', self.input_data)
+    pass
